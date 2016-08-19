28caf606ca322ac7eff2bed06e19cfae21d07ec8
blue0knight <oni_rivera98@yahoo.com>
2016-08-19 16:08:25 -0400

    added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'oni_rivera98@yahoo.com'
+
+  def contact_email(name, email, comment)
+      @name = name
+      @email = email
+      @body = body
+
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
