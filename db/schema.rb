2f5f1372eb135d4380e74d9ded8fb7319fcfc4d5
blue0knight <oni_rivera98@yahoo.com>
2016-08-26 11:45:13 -0400

    reverted to earlier commit, added plan_id and fixed errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160824013728) do
+ActiveRecord::Schema.define(version: 20160826143046) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160824013728) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "stripe_customer_token"
     t.integer  "plan_id"
   end
 
